7256819f60c9b018e0217c6f067fef2a3f0ed2b4
Maksym Hayovets <makcumyt@gmail.com>
2023-11-02 20:26:29 +0200

    add leads types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,3 @@
 export * as DDB from './common-ddb-types';
 export * as Users from './users';
-
+export * as Leads from './leads';
---
+++ packages/types/src/leads.ts
@@ -0,0 +1,28 @@
+export type LeadFacet = 'Lead';
+
+export type DDBLeadFacet = Lead & {
+  hash_key: string;
+  range_key: string;
+  facetType: LeadFacet;
+}
+
+export type Lead = {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  country: string;
+  phonePrefix: string;
+  phone: string;
+  ip: string;
+  createdAt: string;
+  updatedAt?: string;
+  source?: string;
+  funnel?: string;
+  adId?: string;
+  adName?: string;
+  adSetId?: string;
+  adSetName?:string;
+  campainId?: string;
+  campainName?: string;
+}
