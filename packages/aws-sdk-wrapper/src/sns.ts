bc10f434353fc64df97bfc12e71dbd19db95dd8f
Maksym Hayovets <makcumyt@gmail.com>
2023-11-05 13:41:02 +0200

    refactor input params for sns publish helper

--- packages/aws-sdk-wrapper/src/sns.ts
+++ packages/aws-sdk-wrapper/src/sns.ts
@@ -1,13 +1,17 @@
 import {clientSNS, clientSTS} from './clients';
 
+type PublishMessageParams = {
+  topicName: string;
+  messageBody: Record<string, unknown>;
+}
+
 export async function publishMessage(
-    topic: string,
-    message: Record<string, unknown>,
+    {topicName, messageBody}:PublishMessageParams,
 ): Promise<string> {
   const accId = await clientSTS.getAccountId();
-  const TopicArn = `arn:aws:sns:${process.env.AWS_REGION}:${accId}:${topic}`;
+  const TopicArn = `arn:aws:sns:${process.env.AWS_REGION}:${accId}:${topicName}`;
 
-  const {MessageId} = await clientSNS.publish({TopicArn, Message: JSON.stringify(message)});
+  const {MessageId} = await clientSNS.publish({TopicArn, Message: JSON.stringify(messageBody)});
 
   return MessageId as string;
 }
